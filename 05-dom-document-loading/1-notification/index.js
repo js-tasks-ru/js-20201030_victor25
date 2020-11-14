08aaff8be00443b5197789572aca4f7504497781
Victor Alsheuski <v.alsheuski@techin.by>
2020-11-14 10:18:31 +0300

    05-dom-document-loading: Implement NotificationMessage component

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,82 @@
+const SUCCESS_TYPE = 'success';
+const ERROR_TYPE = 'error';
+const SUCCESS_NOTIFICATION_CLASSES = 'notification success';
+const ERROR_NOTIFICATION_CLASSES = 'notification error';
+
 export default class NotificationMessage {
+
+    static activeElement;
+    
+    constructor(msg = '', {
+        duration = 1000,
+        type = SUCCESS_TYPE
+    } = {}) {
+        this._message = msg;
+        this._type = type;
+        this._duration = duration;
+
+        this.render();
+    }
+
+    get duration () {
+        return this._duration;
+    }
+
+    get durationInSec () {
+        return this._duration / 1000;
+    }
+
+    get type () {
+        return this._type;
+    }
+
+    get message () {
+        return this._message;
+    }
+
+    get template() {
+        return `<div class="${this.getNotificationClasses()}" style="--value:${this.durationInSec}s">
+            <div class="timer"></div>
+            <div class="inner-wrapper">
+            <div class="notification-header">${this.type}</div>
+            <div class="notification-body">${this.message}</div>
+            </div>
+        </div>`;
+    }
+
+    render() {
+        const element = document.createElement("div");
+        element.innerHTML = this.template;
+        this.element = element.firstElementChild;
+    }
+
+    show(parent = document.body) {
+        parent.append(this.element);
+        if (NotificationMessage.activeElement) {
+            NotificationMessage.activeElement.remove();
+        }
+        NotificationMessage.activeElement = this.element;
+        setTimeout(this.remove.bind(this), this._duration);
+    }
+
+    remove() {
+        this.element.remove();
+    }
+
+    destroy() {
+        this.remove();
+        NotificationMessage.activeElement = null;
+    }
+
+    getNotificationClasses () {
+        switch (this._type) {
+            case SUCCESS_TYPE:
+                return SUCCESS_NOTIFICATION_CLASSES;
+            case ERROR_TYPE:
+                return ERROR_NOTIFICATION_CLASSES;
+            default:
+                return '';
+        }
+    }
 
 }
