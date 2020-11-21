e7325c3949eeb70b3c93545b303a09aa97f794ca
Victor Alsheuski <v.alsheuski@techin.by>
2020-11-21 21:01:11 +0300

    06-events-practice: Implement Tooltip element

--- 06-events-practice/2-tooltip/index.js
+++ 06-events-practice/2-tooltip/index.js
@@ -1,5 +1,63 @@
 class Tooltip {
 
+  static instance;
+  element;
+
+  constructor() {
+      if (Tooltip.instance) {
+        return Tooltip.instance;
+      }
+
+      Tooltip.instance = this;
+  }
+  
+  initialize() {
+      this.initEventListeners();
+  }
+
+  render(value) {
+    this.element = document.createElement('div');
+    this.element.className = 'tooltip';
+    this.element.innerHTML = value;
+
+    document.body.append(this.element);
+  }
+
+  handlePointerover(event) {
+    if (event.target.dataset.tooltip != undefined) {
+      const element = event.target.closest('[data-tooltip]');
+      const tooltipValue = element.dataset.tooltip;
+      if (element) {
+        this.render(tooltipValue);
+        this.element.style.left = event.clientX + 'px';
+        this.element.style.top = event.clientY + 'px';
+      }
+    }
+  }
+
+  handlePointerout(event) {
+    if (event.target.dataset.tooltip != undefined) {
+      this.remove();
+    }
+  }
+
+  initEventListeners () {
+    document.addEventListener('pointerover', this.handlePointerover.bind(this));
+    document.addEventListener('pointerout', this.handlePointerout.bind(this));
+  }
+
+  remove() {
+    if (this.element) {
+      this.element.remove();
+      this.element = null;
+    }
+  }
+
+  destroy() {
+    this.remove();
+    document.removeEventListener('pointerover', this.handlePointerover.bind(this));
+    document.removeEventListener('pointerout', this.handlePointerout.bind(this)); 
+  }
 }
 
 const tooltip = new Tooltip();
